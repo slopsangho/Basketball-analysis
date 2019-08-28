d2380156bf4f8b4ca91b0cb653ff3d50188c0ecc
SANG HO KIM <sanghokim@SANGui-MacBook-Pro.local>
2019-08-27 18:07:46 -0700

    single file

---
+++ The "While" Loop.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+while False:
+    print("Hello")
+
+
+# In[3]:
+
+
+counter = 0
+while counter < 12:
+    print(counter)
+    counter = counter + 1
+print("Hello")
+
+
+# In[ ]:
+
+
+
+
